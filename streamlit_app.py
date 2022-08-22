66a4ce8448be673469aecbede659738d0ddc6f32
Pritiprakash Nayak <109714227+pritiprakas@users.noreply.github.com>
2022-08-23 00:18:44 +0530

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,3 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega3 & Blueberry Oatmeal')
 streamlit.text('Kale,Spinach and Rocket smoothie')
 streamlit.text('Hard boiled free range egg')  
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-fruits_seleced = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
